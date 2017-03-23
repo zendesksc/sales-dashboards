14a2e8ba38b60c2426c06a560e951b25fc9ea788
Adam Gray <adam@pateo.co>
2017-03-23 12:27:47 +0000

    Create home page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+
+  def index
+    @departments = Department.all
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
   resources :dashboards, except: :show
   get '/departments/:id', to: 'departments#show', as: 'department'
 end
