e1f838eb024e8834c2d889c7b31bcaa6891e4748
Adam Gray <adam@pateo.co>
2017-03-27 15:09:16 +0100

    Add region tests

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,3 +1,4 @@
 class Region < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
   has_many :departments
 end
--- test/models/region_test.rb
+++ test/models/region_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class RegionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @region = Region.new({ name: 'EMEA' })
+  end
+
+  test "should be valid" do
+    assert @region.valid?
+  end
+
+  test "should have name" do
+    @region.name = ''
+    assert_not @region.valid?
+  end
+
+  test "name should be unique" do
+    region1 = Region.create({ name: 'test' })
+    region2 = Region.new({ name: 'test' })
+    assert_not region2.save
+  end
 end
