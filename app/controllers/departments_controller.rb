c124dee138152df575ca7fd0b1e0a73734bc17ed
Adam Gray <adam@pateo.co>
2017-03-28 14:53:19 +0100

    404 departments without any dashboards

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,5 +1,8 @@
 class DepartmentsController < ApplicationController
   def show
     @department = Department.find(params[:id])
+    if @department.dashboards.count == 0
+      raise ActionController::RoutingError, 'Not Found'
+    end
   end
 end
